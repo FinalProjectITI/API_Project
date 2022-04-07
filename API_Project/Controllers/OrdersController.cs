2dbf9177ef23f4740b7ad96b9f5e513a941f2242
beshoy <bbeshoymikhail@gmail.com>
2022-04-08 01:26:59 +0200

    LCB

--- API_Project/Controllers/OrdersController.cs
+++ API_Project/Controllers/OrdersController.cs
@@ -81,14 +81,84 @@ namespace API_Project.Controllers
         // POST: api/Orders
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
+        public async Task<ActionResult<Order>> PostOrder(DtailsOrder oorder)
         {
             string UserName = User.FindFirstValue(ClaimTypes.Name);
             var user_id = _context.AspNetUsers.Where(U => U.UserName == UserName).Select(U => U.Id).FirstOrDefault();
-            order.status = 1;
-            order.UserID = user_id;
-            _context.Orders.Add(order);
-            await _context.SaveChangesAsync();
+            Order order = new Order();
+            Cart c1 = _context.Carts.FirstOrDefault(c => c.UserID == user_id);
+            if(c1!=null)
+            {
+
+                order.status = 1;
+                order.UserID = user_id;
+                order.CartID = c1.ID;
+                order.Address = oorder.Address;
+                order.Phone = oorder.Phone;
+                ////////////////////////////////////////////////botros
+                ///
+                int CartIDD = c1.ID;
+                var ListOfProduct = _context.Product_In_Carts.Where(p => p.CartID == CartIDD).ToList();
+                CartDetalisMV cartview = new CartDetalisMV();
+                List<ProductCartMV> ProductsVCartt = new List<ProductCartMV>();
+                cartview.ProductsVCart = ProductsVCartt;
+
+
+                cartview.TotalCartPrice = 0;
+                foreach (var item in ListOfProduct)
+                {
+
+                    ProductVM productVMM = new ProductVM();
+                    productVMM.Quntity = item.quantity;
+                    productVMM.ID = item.ProductID;
+                    var product = _context.Products.FirstOrDefault(p => p.ID == item.ProductID);
+                    productVMM.Name = product.Name;
+                    productVMM.CategoryID = product.CategoryID;
+                    var categoryT = _context.Categories.FirstOrDefault(c => c.ID == product.CategoryID);
+                    productVMM.Category = categoryT.Name;
+                    productVMM.SeasonID = product.SeasonID;
+                    var seassonT = _context.Seasons.FirstOrDefault(s => s.ID == product.SeasonID);
+                    productVMM.Season = seassonT.Name;
+                    productVMM.Price = (double)(product.Price - product.Discount);
+                    productVMM.Description = product.Description;
+                    productVMM.TypeID = product.TypeID;
+                    var TypeT = _context.Types.FirstOrDefault(T => T.ID == product.TypeID);
+                    productVMM.Type = TypeT.Name;
+                    productVMM.FirstImage = _context.ProductImages
+                            .Where(P => P.ProductID == item.ProductID)
+                            .Select(p => p.ImagePath).FirstOrDefault();
+                    //productVMM.Images = _context.ProductImages
+                    //        .Where(P => P.ProductID == item.ProductID)
+                    //        .Select(p => p.ImagePath).ToList();
+                    //productVMM.ShowInHome = true;
+                    productVMM.Discount = product.Discount;
+
+
+
+
+
+
+                    //product should appear on cart
+                    ProductCartMV productCartMVV = new ProductCartMV();
+                    productCartMVV.ProductVM = productVMM;
+                    productCartMVV.QuntityOfProduct = productVMM.Quntity;
+                    productCartMVV.TotalPrice = (int)(productCartMVV.QuntityOfProduct * productVMM.Price);
+
+
+                    //cart detalis vew
+                    cartview.TotalCartPrice += productCartMVV.TotalPrice;
+                    /* cartview.ProductsVCart.Add(productCartMVV);*/
+                    ProductsVCartt.Add(productCartMVV);
+
+                }
+                /////////////////////////////////
+                order.TotalPrice = cartview.TotalCartPrice;
+                order.PaymentMethod = true;
+               // order.Cart = null;
+                _context.Orders.Add(order);
+               // _context.Carts.Remove(c1);
+                await _context.SaveChangesAsync();
+            }
 
             return Ok("Added Succefuly");
         }
